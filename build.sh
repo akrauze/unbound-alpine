6bbd3010cdaaefff7e6667a9b84b8b55931b49ea
akrauze <krauze@gmail.com>
2023-11-07 19:09:19 -0500

    unbound 1.18

--- build.sh
+++ build.sh
@@ -3,10 +3,9 @@
 echo $CR_PAT | docker login ghcr.io -u USERNAME --password-stdin
 
 build() {
-	UNBOUND_VERSION=${1:=1.16.3-r0}
+	UNBOUND_VERSION=${1:=1.18.0-r0}
 	ALPINE_VERSION=${2:=edge}
-
-	CMD="docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --build-arg UNBOUND_VERSION=$UNBOUND_VERSION --build-arg ALPINE_VERSION=$ALPINE_VERSION -t ghcr.io/akrauze/unbound-alpine:$UNBOUND_VERSION -t ghcr.io/akrauze/unbound-alpine:$ALPINE_VERSION -o type=registry ."
+	CMD="docker build  --build-arg UNBOUND_VERSION=$UNBOUND_VERSION --build-arg ALPINE_VERSION=$ALPINE_VERSION -t ghcr.io/akrauze/unbound-alpine:$UNBOUND_VERSION -t ghcr.io/akrauze/unbound-alpine:$ALPINE_VERSION -o type=registry ."
 
 	echo $CMD
 
@@ -14,5 +13,5 @@ build() {
 }
 
 #build 1.15.0-r0 latest
-build 1.16.3-r0 edge
+build 1.18.0-r0 edge
 
